50cf20cea239af56fb07a2ece7f4792cc27ccfbc
Nick <nbastida@treasuryspring.com>
2023-11-17 19:13:03 +0000

    small fix in migrate script

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Waiting for PostgreSQL to be ready..."
-./wait-for-it.sh postgres:5432 -t 5
+./wait-for-it.sh db:5432 -t 5
 
 echo "Running migrations..."
 DB=$PROD_DBNAME alembic -c /alembic.ini upgrade head
